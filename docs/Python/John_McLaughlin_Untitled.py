98db486ef6ca35da995e7a7768004769a0a263f1
Max Farrell <maxleonardfarrell1@gmail.com>
2015-09-30 18:57:08 -0400

    Reorganization and More Python Conversions

---
+++ docs/Python/John_McLaughlin_Untitled.py
@@ -0,0 +1,11 @@
+def setup():
+    size (960, 703);
+    background(227, 226, 224);
+    noStroke();
+def draw():
+    rectMode(CENTER);
+    fill(148, 51, 45);
+    rect(258, 339, 390, 597);
+    rectMode(CENTER);
+    fill(108, 175, 202);
+    rect(698, 339, 390, 597);
